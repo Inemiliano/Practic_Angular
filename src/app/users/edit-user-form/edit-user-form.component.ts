e018d475df71748eb2237b807e476abdefa11bfd
gilberto <emigil3010@gmail.com>
2024-09-17 22:56:05 -0600

    ultimo commit

--- src/app/users/edit-user-form/edit-user-form.component.ts
+++ src/app/users/edit-user-form/edit-user-form.component.ts
@@ -1,32 +1,58 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
-import { UserService } from '../services/user.service';
 import { IUser } from '../interfaces/iuser';
 
 @Component({
   selector: 'app-edit-user-form',
   templateUrl: './edit-user-form.component.html',
-  styleUrl: './edit-user-form.component.css'
+  styleUrls: ['./edit-user-form.component.css'] 
 })
 export class EditUserFormComponent {
-
   @Input() user: IUser = {
     id: 0,
-    name: "",
-    username: "",
-    email: "",
-    phone: "",
-    website: ""
+    name: '',
+    username: '',
+    email: '',
+    phone: '',
+    website: ''
+  };
+
+  @Input() disabled: boolean = true; 
+
+  @Output() eventEmitter = new EventEmitter<IUser>(); 
+
+  nuevoUsuario: IUser = {
+    id: 0,
+    name: '',
+    username: '',
+    email: '',
+    phone: '',
+    website: ''
+  };
+
+  usuarios: IUser[] = [];
+
+  constructor() {}
+
+  agregarUsuario(): void {
+    if (this.nuevoUsuario.name && this.nuevoUsuario.username && this.nuevoUsuario.email) {
+      this.usuarios.push({ ...this.nuevoUsuario });
+      console.log('Usuario agregado:', this.nuevoUsuario);
+      
+      this.eventEmitter.emit(this.nuevoUsuario);
+
+      
+      this.nuevoUsuario = {
+        id: 0,
+        name: '',
+        username: '',
+        email: '',
+        phone: '',
+        website: ''
+      };
+      
+      
+    } else {
+      console.error('Formulario incompleto');
+    }
   }
-
-  @Input() disabled: boolean = true
-
-  @Output() eventEmitter = new EventEmitter<IUser>()
-
-  constructor(private _service: UserService){
-  }
-
-  enviar(): void{
-    this.eventEmitter.emit(this.user);
-  }
-  
 }
